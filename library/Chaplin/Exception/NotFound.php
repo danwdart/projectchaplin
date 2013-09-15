2c1f337f75bd3a5927d98226da5ac0da5deda90e
Dan <git@dandart.co.uk>
2013-09-15 17:44:36 +0100

    Better errors

---
+++ library/Chaplin/Exception/NotFound.php
@@ -0,0 +1,11 @@
+<?php
+class Chaplin_Exception_NotFound
+	extends Exception
+{
+	const MESSAGE = 'Page not found (%s)';
+
+	public function __construct($strIdentifier)
+	{
+		parent::__construct(sprintf(self::MESSAGE, $strIdentifier));
+	}
+}
