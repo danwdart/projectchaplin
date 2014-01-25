fb65527a0ec2f89e7aec0f3025883f51bbacca67
Dan Dart <dandart@googlemail.com>
2014-01-25 23:19:09 +0000

    Composerify

--- public/index.php
+++ public/index.php
@@ -25,6 +25,10 @@
 /**
  * This file was almost completely stolen from Zend Framework, licensed under MIT.
  */
+
+// Include classmap
+require '../vendor/autoload.php';
+
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
@@ -32,12 +36,6 @@ defined('APPLICATION_PATH')
 // Define application environment
 defined('APPLICATION_ENV')
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
 
 /** Zend_Application */
 require_once 'Zend/Application.php';
