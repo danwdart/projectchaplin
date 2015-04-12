f77f15aab20a20eacb69ac6f131018daf6fc5f0e
Dan <git@dandart.co.uk>
2015-04-12 13:24:58 +0100

    use appcache

--- public/js/script.js
+++ public/js/script.js
@@ -67,3 +67,11 @@ $(document).ready(function() {
         }
     });
 });
+
+window.addEventListener('load', function(e) {
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            window.location.reload();
+        }
+    }, false);
+}, false);
