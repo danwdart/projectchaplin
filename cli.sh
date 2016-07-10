39b213295d46d99c13e7396bfe0d7c06bd02e898
Dan <git@dandart.co.uk>
2016-07-10 18:35:36 +0100

    pid related stuff and more debug

--- cli.sh
+++ cli.sh
@@ -3,18 +3,51 @@ case $1 in
     start)
         cd cli
         php cli.php cli youtube > logs/youtube.log & 2>&1
-        php cli.php cli convert > logs/convert.log & 2>&2
+        echo $! > ../youtube.pid
+        php cli.php cli convert > logs/convert.log & 2>&1
+        echo $! > ../convert.pid
         cd ..
         node node/server.js > logs/node.log & 2>&1
-        $0 log
+        echo $! > node.pid
         ;;
     stop)
-        ps aux | grep cli.php | awk '{print $2}' | xargs kill
-        ps aux | grep server.js | awk '{print $2}' | xargs kill
+        kill $(cat youtube.pid) && rm youtube.pid
+        kill $(cat convert.pid) && rm convert.pid
+        kill $(cat node.pid) && rm node.pid
+    ;;
+    status-youtube)
+        if [[ "2" == "$(ps aux | grep $(cat youtube.pid) | wc -l)" ]]
+        then
+            echo YouTube listener running.
+            exit 0
+        else
+            echo YouTube listener not running.
+            exit 1
+        fi
+    ;;
+    status-convert)
+        if [[ "2" == "$(ps aux | grep $(cat convert.pid) | wc -l)" ]]
+        then
+            echo Convert listener running.
+            exit 0
+        else
+            echo Convert listener not running.
+            exit 1
+        fi
+    ;;
+    status-node)
+        if [[ "2" -eq "$(ps aux | grep $(cat node.pid) | wc -l)" ]]
+        then
+            echo Node listener running.
+            exit 0
+        else
+            echo Node listener not running.
+            exit 1
+        fi
     ;;
     restart)
-        $0 start
         $0 stop
+        $0 start
     ;;
     log)
         tail -f logs/youtube.log logs/convert.log logs/node.log
