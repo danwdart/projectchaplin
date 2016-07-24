bebb0d18fc6e432ce1de33f406a8b577165df184
Dan <git@dandart.co.uk>
2016-07-25 00:54:50 +0100

    CLI shell actions

--- cli.sh
+++ cli.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
+cd $(dirname $0)
+CPID=/tmp/convert.pid
+YPID=/tmp/youtube.pid
+NPID=/tmp/node.pid
 case $1 in
     start)
-        php cli/cli.php cli youtube > logs/youtube.log & 2>&1
-        echo $! > ../youtube.pid
-        php cli/cli.php cli convert > logs/convert.log & 2>&1
-        echo $! > ../convert.pid
-        node node/server.js > logs/node.log & 2>&1
-        echo $! > node.pid
+        php cli/cli.php cli youtube > logs/youtube.log 2>&1 &
+        echo $! > $YPID
+        php cli/cli.php cli convert > logs/convert.log 2>&1 &
+        echo $! > $CPID
+        node node/server.js > logs/node.log 2>&1 &
+        echo $! > $NPID
+        echo All services started
         ;;
     stop)
-        kill $(cat youtube.pid) && rm youtube.pid
-        kill $(cat convert.pid) && rm convert.pid
-        kill $(cat node.pid) && rm node.pid
+        kill $(cat $YPID) && rm $YPID
+        kill $(cat $CPID) && rm $CPID
+        kill $(cat $NPID) && rm $NPID
+        echo All services stopped
+    ;;
+    status)
+        if [[ "2" == "$(ps aux | grep $(cat $YPID) | wc -l)"]] &&
+            [["2" == "$(ps aux | grep $(cat $CPID) | wc -l)"]] &&
+            [["2" -eq "$(ps aux | grep $(cat $NPID) | wc -l)" ]]
+        then
+            echo All listeners running.
+            exit 0
+        else
+            echo Some or all listeners not running.
+            exit 1
+        fi
     ;;
     status-youtube)
         if [[ "2" == "$(ps aux | grep $(cat youtube.pid) | wc -l)" ]]
@@ -51,6 +69,6 @@ case $1 in
         tail -f logs/youtube.log logs/convert.log logs/node.log
     ;;
     *)
-        echo "Usage: $0 start|stop|restart|log"
+        echo "Usage: $0 start|stop|restart|status|status-youtube|status-convert|status-node|log"
     ;;
 esac
