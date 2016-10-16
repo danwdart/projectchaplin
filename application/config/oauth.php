8d914f97f95f3f3713d70dfc03433ae6a35d27d6
Dan <git@dandart.co.uk>
2016-10-16 19:24:54 +0100

    fix oauth vhost eliminating any more mixed mode errors

--- application/config/oauth.php
+++ application/config/oauth.php
@@ -8,7 +8,7 @@ return [
         'client_secret' => 'csIFAlwR65VATyk-hs9P2aId',
         'scope' => 'openid%20email%20profile',
         'auth_uri' => 'https://accounts.google.com/o/oauth2/auth',
-        'redirect_uri' => 'http://'.$strVhost.'/login/oauth/provider/google',
+        'redirect_uri' => $strVhost.'/login/oauth/provider/google',
         'token_uri' => 'https://accounts.google.com/o/oauth2/token',
         'info_uri' => 'https://www.googleapis.com/oauth2/v3/userinfo',
         'callback_decode' => ['Zend_Json','decode'],
@@ -24,7 +24,7 @@ return [
         'client_secret' => 'a9756399b811b37fdbcaacc3ec185dfa',
         'scope' => 'email',
         'auth_uri' => 'https://www.facebook.com/dialog/oauth',
-        'redirect_uri' => 'http://'.$strVhost.'/login/oauth/provider/facebook',
+        'redirect_uri' => $strVhost.'/login/oauth/provider/facebook',
         'token_uri' => 'https://graph.facebook.com/oauth/access_token',
         'info_uri' => 'https://graph.facebook.com/me',
         'callback_decode' => function($str){parse_str($str,$arr);return $arr;},
@@ -36,7 +36,7 @@ return [
     'twitter' => [
         'registration_url' => 'https://dev.twitter.com/apps',
         'oauth_version' => 1,
-        'callbackUrl' => 'http://'.$strVhost.'/login/oauth/provider/twitter',
+        'callbackUrl' => $strVhost.'/login/oauth/provider/twitter',
         'siteUrl' => 'https://api.twitter.com/oauth',
         'consumerKey' => 'EFlUeaMDcqnXAJYLqX7jQ',
         'consumerSecret' => 'wjxSWjDhRPslwnjGKil7ocsyovomhw5Bx6cznzW4Ag',
@@ -47,7 +47,7 @@ return [
     // TODO update to statusnet
     'identica' => [
         'oauth_version' => 1,
-        'callbackUrl' => 'http://'.$strVhost.'/login/oauth/provider/identica',
+        'callbackUrl' => $strVhost.'/login/oauth/provider/identica',
         'siteUrl' => 'https://identi.ca/api/oauth',
         'consumerKey' => 'b9fbdb19e1bd55b4d1688e1def2bd1c8',
         'consumerSecret' => 'ef78ff72983c3681dd5158b1da8efe7c',
