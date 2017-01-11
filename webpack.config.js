62f7e152d969236808339df73de8b8bac18eaa99
Kathie Dart <git@kathiedart.uk>
2017-01-11 22:28:43 +0000

    Some compiles

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    // configuration
+   entry: __dirname+"/public/js/src/index.js",
+   output: {
+       path: __dirname+'/public/js',
+       filename: "index.js"
+   },
+   module: {
+       loaders: [
+           { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+           { test: /\.css$/, loader: "style!css" }
+       ]
+   }
 };
