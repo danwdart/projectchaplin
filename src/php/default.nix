a633c83225a8ab805b64aeed6f9aeac69447c9f3
Dan Dart <git@dandart.co.uk>
2021-03-21 12:00:52 +0000

    nixpkgs master

--- src/php/default.nix
+++ src/php/default.nix
@@ -1,4 +1,4 @@
-{pkgs ? import <nixpkgs> {
+{pkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz) {
     inherit system;
   }, system ? builtins.currentSystem, noDev ? false}:
 
